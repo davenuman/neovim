80992856ef629b1361ccf81f1f1bc3d1b79b840e
David Numan <davidnuman@gmail.com>
2024-11-13 16:44:17 -0500

    Adding helm, go, and allowing keymaps H and L.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.php" },
+    { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "plugins" },
   },
   defaults = {
