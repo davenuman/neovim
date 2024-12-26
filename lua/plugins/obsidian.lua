f2dd7b12dfe951f81afb3e710ec83670256fabd8
David H Numan <davenuman@users.noreply.github.com>
2024-12-26 18:59:38 -0500

    Markdown and Obsidian config.

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,19 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Notes",
+        path = "~/Obsidian/Knowledge",
+        strict = true,
+      },
+    },
+    ui = { enable = false },
+  },
+}
