331320fb14637a5bdf147cbc8f66b04a54bb51e7
David Numan <davidnuman@gmail.com>
2025-04-06 13:50:54 -0400

    Correcting obsidian config and adding key bindings.
    - update lock file

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,5 +1,5 @@
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = false,
   ft = "markdown",
@@ -11,9 +11,83 @@ return {
       {
         name = "Notes",
         path = "~/Obsidian/Knowledge",
-        strict = false,
+        -- strict = false,
       },
     },
     ui = { enable = false },
+    notes_subdir = "Notes",
+    daily_notes = {
+      folder = "Journal",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d-%a",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%A %B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "journal" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
+    templates = {
+      -- folder = "Templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+    },
+
+    -- Either 'wiki' or 'markdown'.
+    preferred_link_style = "wiki",
+
+    -- picker = {
+    --   name = "telescope.nvim",
+    -- },
+
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Enables completion using nvim_cmp
+      nvim_cmp = false,
+      -- Enables completion using blink.cmp
+      blink = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- -- Smart action depending on context, either follow link or toggle checkbox.
+      -- ["<cr>"] = {
+      --   action = function()
+      --     return require("obsidian").util.smart_action()
+      --   end,
+      --   opts = { buffer = true, expr = true },
+      -- }
+    },
+
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.fn.jobstart({ "qutebrowser", url })
+    end,
   },
+  -- vim.keymap.del("n", "<leader>n"),
+  vim.keymap.set("n", "<leader>n", "", { desc = "Note system" }),
+  vim.keymap.set("n", "<leader>nb", ":ObsidianBacklinks<CR>", { desc = "Show Backlinks" }),
+  vim.keymap.set("n", "<leader>nd", ":ObsidianDailies<CR>", { desc = "Daily Notes" }),
+  vim.keymap.set("n", "<leader>nn", ":ObsidianNew", { desc = "Obsidian New" }),
+  vim.keymap.set("n", "<leader>no", ":ObsidianOpen<CR>", { desc = "Open in Obsidian" }),
+  vim.keymap.set("n", "<leader>ns", ":ObsidianSearch<CR>", { desc = "Obsidian Search" }),
+  vim.keymap.set("n", "<leader>nt", ":ObsidianTags<CR>", { desc = "Obsidian Tags" }),
+  vim.keymap.set("n", "<leader>ngs", ":ObsidianFollowLink hsplit<CR>", { desc = "Follow Link split" }),
+  vim.keymap.set("n", "<leader>ngv", ":ObsidianFollowLink vsplit<CR>", { desc = "Follow Link vsplit" }),
+  vim.keymap.set("v", "<leader>o", ":ObsidianExtractNote<CR>", { desc = "Extract Note" }),
 }
--- lua/plugins/parrot.lua
+++ lua/plugins/parrot.lua
@@ -39,4 +39,19 @@ return {
       },
     })
   end,
+
+  vim.keymap.set("n", "<leader>p", "", { desc = "Parrot LLM" }),
+  vim.keymap.set("n", "<leader>pn", ":PrtChatNew<CR>", { desc = "Parrot Chat: New" }),
+  vim.keymap.set("n", "<leader>pf", ":PrtChatFinder<CR>", { desc = "Parrot Chat: Find" }),
+  vim.keymap.set("n", "<leader>pr", ":PrtChatResponde", { desc = "Parrot Chat: Respond" }),
+  vim.keymap.set("n", "<leader>pi", ":PrtImplement", { desc = "Parrot Implement" }),
+  vim.keymap.set("n", "<leader>pa", ":PrtAsk", { desc = "Parrot Ask" }),
+  vim.keymap.set("n", "<leader>pp", ":PrtProvider<CR>", { desc = "Parrot Provider" }),
+  vim.keymap.set("n", "<leader>pP", ":PrtChatPaste<CR>", { desc = "Parrot Chat: Paste" }),
+  vim.keymap.set("n", "<leader>pt", ":PrtChatToggle<CR>", { desc = "Parrot Chat: Toggle" }),
+  vim.keymap.set("n", "<leader>pM", ":PrtModel<CR>", { desc = "Parrot Model" }),
+  vim.keymap.set("n", "<leader>pw", ":PrtRewrite<CR>", { desc = "Parrot Rewrite" }),
+  vim.keymap.set("n", "<leader>pc", ":PrtEdit<CR>", { desc = "Parrot Edit" }),
+  vim.keymap.set("n", "<leader>po", ":PrtAppend<CR>", { desc = "Parrot Append" }),
+  vim.keymap.set("n", "<leader>pO", ":PrtPrepend<CR>", { desc = "Parrot Prepend" }),
 }
